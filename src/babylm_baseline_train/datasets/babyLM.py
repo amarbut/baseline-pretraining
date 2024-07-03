c619126b582499726ecc40ca8bd532fbc83b8890
amarbut <anna.marbut@gmail.com>
2024-07-03 13:18:48 -0600

    fixing tab/space indents

--- src/babylm_baseline_train/datasets/babyLM.py
+++ src/babylm_baseline_train/datasets/babyLM.py
@@ -18,10 +18,10 @@ class BabyLM(BaseGroupDataset):
         self.name = name
 
     def get_dataset(self):
-    	if self.name in ["babyLM-10M", "babyLM-100M"]:
-    		loader = self.name
-    	else:
-    		loader = "babyLM-10M"
+        if self.name in ['babyLM-10M', 'babyLM-100M']:
+            loader = self.name
+        else:
+            loader = 'babyLM-10M'
         self.dataset = hf_loaders.get_babyLM(
                 name=loader,
                 split="train")
